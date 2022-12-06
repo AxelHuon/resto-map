3dab5143b86f68a0403974cb1b5224e9dd4a3a43
Pierre Antoine Baizeau <pabaizeau@gmail.com>
2022-12-06 14:19:35 +0100

    [resto-map-1][FEATURE]:init react leaflet map

--- src/views/Room/Partials/RoomMap.js
+++ src/views/Room/Partials/RoomMap.js
@@ -2,29 +2,24 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { Icon } from 'leaflet'
+import '../../../style/main.css';
 
 const RoomMap = () => {
 
-    const position = [51.505, -0.09]
-
+    const position = [48.893139, 2.226910]
       const customIcon = new Icon({
         iconUrl: "https://static.vecteezy.com/system/resources/previews/010/160/458/original/pin-location-icon-sign-symbol-design-free-png.png",
-        iconSize: [33, 33]
+        iconSize: [20, 30]
       })
     
 	return (
         <div className="map" id="map" >
-         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker icon={customIcon} position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+          <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            <Marker icon={customIcon} position={position}>
+              <Popup>Nom du Restaurant/ user/point d'arrivé</Popup>
+            </Marker>
+          </MapContainer>
         </div>
 	);
 };
