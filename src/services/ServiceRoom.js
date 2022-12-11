f89e307c26575211f1a5a1fb808faf11904e3ed5
Pierre Antoine Baizeau <pabaizeau@gmail.com>
2022-12-11 18:13:23 +0100

    add room

--- src/services/ServiceRoom.js
+++ src/services/ServiceRoom.js
@@ -1,19 +1,33 @@
-const rooms = [{
-	id: "1",
-	finalPoint: {
-		long: 48.890011, lat: 2.197020,
-	},
-
-},]
-
+const rooms = [
+  {
+    id: "1",
+    finalPoint: {
+      long: 48.890011,
+      lat: 2.19702,
+    },
+  },
+  {
+    id: "2",
+    finalPoint: {
+      long: 48.890011,
+      lat: 2.19702,
+    },
+  },
+  {
+    id: "3",
+    finalPoint: {
+      long: 48.890011,
+      lat: 2.19702,
+    },
+  },
+];
 
 export const getRoom = (roomId) => {
-	let room = {}
-	rooms.map((item) => {
-		if (item.id === roomId) {
-			room = item
-		}
-	})
-	return room
-}
-
+  let room = {};
+  rooms.map((item) => {
+    if (item.id === roomId) {
+      room = item;
+    }
+  });
+  return room;
+};
